ce130ca1a85275c259e0f8d2a5ea6753fc8d2d45
Starodvorski <165729272+Starodvorski@users.noreply.github.com>
2024-04-05 08:31:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const words = ["Шлёпнуть", "Смять", "Хрустнуть", "Проследить", "Скрутить", "Очертить", "Завояжить", "Дать в", "Расквасить", "Сорвать", "Развернуть", "Отправить в", "Въебать в", "Раскусить", "Вынести", "Раздерибанить", "Распидорасить", "Абстрагировать", "Вскрыть", "Припиздячить", "Вырвать", "Порвать", "Распиздячить", "Оформить"];
+const words2 = ["кабинет ", "казинак ", "бобину ", "табло ", "рапсодию ", "периметр  ", "кибитку ", "сертаки ", "парапет ", "пломбу ", "чердак ", "Барановичи ", "аврал ", "герменевтику ", "капитал ", "Чебоксары ", "мачту ", "ебало ", "пилораму ", "кавардак ", "фуникулёр ", "киянку ", "кадило ", "декрет "];
+
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomWord = words[randomIndex];
+  document.getElementById("random-word").innerText = randomWord;
+  
+  const randomIndex2 = Math.floor(Math.random() * words2.length);
+  const randomWord2 = words2[randomIndex2];
+  document.getElementById("random-word2").innerText = randomWord2;
+}
